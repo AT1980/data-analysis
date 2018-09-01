cdebece2f2aea8928b4e93743afb08259dfbd62f
AT1980 <amiteshtyagi@gmail.com>
2018-09-01 06:35:30 +0530

    Create twitter_data_analysis.py

---
+++ twitter_data_analysis.py
@@ -0,0 +1,4 @@
+consumer_key = 'hD98CV5uD4rFVsYuBk8xAzpLd'
+consumer_secret = '	3jrN58VAI6ZuyAmicMReMdmOPjM2o0WKaBz1FWzhFQ8TfNblkS'
+access_token = '81300781-vHqEdU2fgJLalyrFzsRscRAgb9zEZYC9giIzQgnoZ'
+access_token_secret = 'uliqVE0Ofv8sWfWGu97jUHKceLpQm8Ky4waVwKO5KzV2E'
